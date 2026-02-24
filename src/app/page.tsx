4ea04dc5a1731ca13c6c8ad279645267f2b6a4b2
Matheus Rodrigues <theusgd@gmail.com>
2026-02-24 18:55:16 +0000

    a rota nao pode ser estatica

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { FusionReactorDashboard } from '@/components/dashboard/fusion-reactor-dashboard';
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <main>
