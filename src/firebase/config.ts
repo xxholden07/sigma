0a895943cf9132b8a58719bff6998e659a8aad4d
Matheus Rodrigues <theusgd@gmail.com>
2026-02-25 17:53:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8560845051-9a319",
+  "appId": "1:391636410996:web:7f18189777c1e36dfa6721",
+  "apiKey": "AIzaSyBuNh7mKZ2JeD51uxRfAgxAIMOkIgWP-Ec",
+  "authDomain": "studio-8560845051-9a319.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "391636410996"
+};
